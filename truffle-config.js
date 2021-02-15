e9678e5a2f1fb76135e564c775d5f7a56a3984e6
epocopa <epocopa@gmail.com>
2021-02-15 18:12:33 +0100

    Make truffle use default network config

--- truffle-config.js
+++ truffle-config.js
@@ -5,11 +5,12 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    development: {
+    dev: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
-    },
+      network_id: "*", // Match any network id
+      websockets: true
+    }
   },
   compilers: {
     solc: {
