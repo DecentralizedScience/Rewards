db63888210a06d24b54a0996c6028cae1e668089
epocopa <epocopa@gmail.com>
2021-02-18 20:27:33 +0100

    Rework rewards contract

--- contracts/Rewards.sol
+++ contracts/Rewards.sol
@@ -2,26 +2,27 @@ pragma solidity ^0.8.0;
 
 contract Rewards {
     uint public paperCount = 0;
-    mapping(uint => Paper) public papers;
-
+    mapping(string => bool) public hasGivenReputation;//hash
 
     struct Paper {
         uint id;
         string title;
         uint tipAmount;
         address author;
-        address payable reviewer;
+        uint reviewerCount;
+        mapping(address => string) reviewers;
     }
+
+    Paper[] public papers;
 
     event PaperCreated(
         uint id,
         string title,
         uint tipAmount,
-        address author,
-        address payable reviewer
+        address author
     );
 
-    event PaperTipped(
+    event reviewerTipped(
         uint id,
         string title,
         uint tipAmount,
@@ -29,23 +30,29 @@ contract Rewards {
         address payable reviewer
     );
 
-    function createPaper(string memory _title, address payable _reviewer) public {
+    function createPaper(string memory _title) public {
         require(bytes(_title).length > 0);
-        papers[paperCount] = Paper(paperCount, _title, 0, msg.sender, _reviewer);
-        emit PaperCreated(paperCount, _title, 0, msg.sender, _reviewer);
+        Paper storage paper = papers.push();
+        paper.id = paperCount;
+        paper.title = _title;
+        paper.author = msg.sender;
+        emit PaperCreated(paperCount, _title, 0, msg.sender);
         paperCount++;
     }
 
-    function tipPaper(uint _id) public payable{
+    function addReviewer(uint _id, address payable _reviewer, string memory review) public {
         require(_id < paperCount);
-        Paper memory _paper = papers[_id];
+        papers[paperCount].reviewers[_reviewer] = review;
+        //TODO emit Reviewer added
+        papers[paperCount].reviewerCount++;
+    }
 
-        address payable _reviewer = _paper.reviewer;
-        _paper.tipAmount += msg.value;
+    function tipReviewer(uint _id, address payable _reviewer) public payable{
+        require(_id < paperCount);
 
+        papers[_id].tipAmount += msg.value;
         _reviewer.transfer(msg.value);
 
-        papers[_id] = _paper;
-        emit PaperTipped(paperCount, _paper.title, _paper.tipAmount, _paper.author, _reviewer);
+        emit reviewerTipped(paperCount, papers[_id].title, msg.value, papers[_id].author, _reviewer);
     }
 }
